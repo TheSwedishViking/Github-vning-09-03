c31ba5e8c9c39db236e59e83e789b74d0f0b0402
Robin Bertling <robinbertling10@gmail.com>
2026-09-03 13:52:33 +0200

    added object in use

--- ToDoAppTest/Program.cs
+++ ToDoAppTest/Program.cs
@@ -4,8 +4,24 @@
     {
         static void Main(string[] args)
         {
+            int id = 1;
             Console.WriteLine("Hello, Oscar!");
             Console.WriteLine("Today is great!");
+
+
+            Console.WriteLine("Tasks to do");
+            string title = Console.ReadLine();
+
+            Models.Task task = new Models.Task
+            {
+                Title = title,
+                IsCompleted = false,
+                Id = id
+            };
+
+
+
+
         }
     }
 }
