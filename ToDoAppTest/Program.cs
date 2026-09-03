3b97f533dfc71390d464ec1e5740325067693adb
Oscar Stenström <oscar.stenstrom2001@gmail.com>
2026-09-03 13:57:08 +0200

    List all tasks as either completed or not

--- ToDoAppTest/Program.cs
+++ ToDoAppTest/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             int id = 1;
+            List<Models.Task> tasks = new List<Models.Task>();
             Console.WriteLine("Hello, Oscar!");
             Console.WriteLine("Today is great!");
 
@@ -18,7 +19,22 @@
                 IsCompleted = false,
                 Id = id
             };
+            tasks.Add(task);
 
+            foreach(var t in tasks)
+            {
+                if (t.IsCompleted)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkCyan;
+                }
+
+                Console.WriteLine($"Task {t.Id}: {t.Title} - Completed: {t.IsCompleted}");
+            }
+            Console.ForegroundColor = ConsoleColor.White;
 
 
 
