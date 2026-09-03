8511df720b2102a46e755a1bb39e99f69f5e8178
Robin Bertling <robinbertling10@gmail.com>
2026-09-03 13:25:51 +0200

    nyttProjekt

---
+++ ToDoAppTest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoAppTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Oscar!");
+        }
+    }
+}
